cb42d528ae290647bf159212da1c524f80a6f8bf
Tomasz Bęc <tomasz.bec@gmail.com>
2026-03-18 08:46:45 +0100

    CF change

--- src/index.php
+++ src/index.php
@@ -1,4 +1,6 @@
 <?php
 // PHP Router for SPA (always include the main phtml entry point)
+// Allow Cloudflare to cache for a long time (1 day), but keep browser cache shorter (1 hour)
+header("Cache-Control: public, max-age=3600, s-maxage=86400");
 include_once("index.phtml");
 ?>
